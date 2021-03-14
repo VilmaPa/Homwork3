9e81950eb1477a21dd8564cfb034680cd9edf081
VilmaPa <70938111+VilmaPa@users.noreply.github.com>
2021-03-14 19:10:25 +0200

    Add project files.

---
+++ Models/Albums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homwork3.Models
+{
+    class Albums
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Homwork3.Models;
+using System.Collections.Generic;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace Homwork3
+{
+    class Program
+    {
+        
+
+        static async Task Main(string[] args)
+        {
+
+                var httpClient = new HttpClient();
+                var httpResponse = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+                var httpClient2 = new HttpClient();
+                var httpResponse2 = await httpClient2.GetAsync("https://jsonplaceholder.typicode.com/albums");
+                var httpClient3 = new HttpClient();
+                var httpResponse3 = await httpClient3.GetAsync("https://jsonplaceholder.typicode.com/photos");
+
+            if ((httpResponse.StatusCode == System.Net.HttpStatusCode.OK)&&(httpResponse2.StatusCode == System.Net.HttpStatusCode.OK)&&(httpResponse3.StatusCode == System.Net.HttpStatusCode.OK))
+            {
+                    var contentString = await httpResponse.Content.ReadAsStringAsync();
+                    var users = JsonConvert.DeserializeObject<List<Users>>(contentString);
+                    var DennisUserId = users.Where(u => u.Name == "Mrs. Dennis Schulist").Select(u => u.Id).FirstOrDefault();
+                   
+                    var contentString2 = await httpResponse2.Content.ReadAsStringAsync();
+                    var albums = JsonConvert.DeserializeObject<List<Albums>>(contentString2);
+                //result is list
+                    var filteredAlbumId = albums.Where(a => a.UserId == DennisUserId).Select(a => a.Id).ToList();
+
+                    var contentString3 = await httpResponse3.Content.ReadAsStringAsync();
+                    var photos = JsonConvert.DeserializeObject<List<Photos>>(contentString3);
+
+                    var filteredPhotos = photos.Where(p => filteredAlbumId.Contains(p.AlbumId)).Select(p => p.Url).ToList();
+                  
+                    foreach (string photo in filteredPhotos)
+                    {
+                            Console.WriteLine(photo);
+                    }
+            }
+
+
+            
+        }
+    }
+}
